ad85d663ac90965060996b0e2a4f061a8f9f50a5
yugendar2006 <ymaladri5@gmail.com>
2026-04-03 09:08:53 +0530

    Add files via upload

---
+++ first_program.py
@@ -0,0 +1,20 @@
+print("Hello world!")
+print("Iam yugendar")
+
+name=input("Enter your name:")
+print(name)
+
+name = input("Enter your name: ")
+print("Hello,", name, "! Welcome!")
+
+y="yugendar"
+print(y)
+
+y="yugendar"
+age=20
+s="iam hungry"
+print(y, age, s)
+
+x ,y = input("Enter two numbers: ").split()
+print(x ,y)
+

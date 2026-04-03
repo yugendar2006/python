2364cee167627f094e39078672a9c993675dae48
yugendar2006 <ymaladri5@gmail.com>
2026-04-03 17:56:20 +0530

    Update first_program.py

--- first_program.py
+++ first_program.py
@@ -18,3 +18,22 @@ print(y, age, s)
 x ,y = input("Enter two numbers: ").split()
 print(x ,y)
 
+x, y, z=10, "string", 1.56
+print(x, y, z)
+
+a =100
+print(type(a))
+
+b=10.5
+print(type(b))
+
+c=99765232137
+print(type(c))
+
+d="string"
+print(type(d))
+
+a=100
+b=float(a)
+print(a)
+print(b)

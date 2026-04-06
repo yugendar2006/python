b6237dbb09c7c465afd0b76a85b625f2d99806f5
yugendar2006 <ymaladri5@gmail.com>
2026-04-06 06:26:50 +0530

    Add input and output examples in input_output.py

---
+++ input_output.py
@@ -0,0 +1,16 @@
+# input statements
+name = input()
+print("My name is", name)
+
+number = input("Enter a number: ")
+print(type(number))
+a = int(number)
+print(type(a))
+number = int(input("Enter a number:"))
+print(type(number))
+b = float(number)
+print(type(b))
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+print("Hello!", name, "your age is", age)

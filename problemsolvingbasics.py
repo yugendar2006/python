04f9067ab8ab17e33243520df5acbd6672ab62c4
yugendar2006 <ymaladri5@gmail.com>
2026-04-08 07:43:10 +0530

    Add files via upload

---
+++ problemsolvingbasics.py
@@ -0,0 +1,8 @@
+num1 = int(input())
+num2 = int(input())
+print("Sum:",num1+num2)
+print(f"Sum:{num1+num2}")
+
+#Finding the area of circle
+radius = float(input("Enter the radius:"))
+print("Area o circle: ",3.14*radius*radius)

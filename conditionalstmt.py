2a4147eab357aaea26c688a4fc35be96df14ec18
yugendar2006 <ymaladri5@gmail.com>
2026-04-09 07:33:27 +0530

    Add files via upload

---
+++ conditionalstmt.py
@@ -0,0 +1,10 @@
+age = int(input("Enter youre age" ))
+if (age >= 18):
+    print("You are eligible to vote")
+
+#if-else
+age = 100
+if age <= 12:
+    print("Travel for free.")
+else:
+    print("Pay for ticket.")

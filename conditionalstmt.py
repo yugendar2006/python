f30c922a4ee43b136f0f8a4c995dc60f8acf709f
yugendar2006 <ymaladri5@gmail.com>
2026-04-10 06:33:33 +0530

    Add conditional statements for age evaluation

--- conditionalstmt.py
+++ conditionalstmt.py
@@ -8,3 +8,26 @@ if age <= 12:
     print("Travel for free.")
 else:
     print("Pay for ticket.")
+
+#elif
+age = 2
+if age <= 12:
+    print("Child.")
+elif age <= 19:
+    print("Teenager.")
+elif age <= 35:
+    print("Young adult.")
+else:
+    print("Adult.")
+
+#Nested if
+age = 20
+is_member = True
+
+if age >= 60:
+    if is_member:
+        print("30% senior discount!")
+    else:
+        print("20% senior discount.")
+else:
+    print("Not eligible for a senior discount.")

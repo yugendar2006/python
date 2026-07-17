e0121e8bd8c477943904686967635259198ab655
yugendar2006 <ymaladri5@gmail.com>
2026-07-17 18:22:13 +0530

    Add files via upload

---
+++ arithmetic_operators.py
@@ -0,0 +1,24 @@
+#static problem
+#model 1 static variables
+num1 = 10
+num2 = 5
+print(f"Addition: ({num1 + num2}):",num1 + num2)
+print(f"Addition: {num1 + num2}")
+
+#f string
+a = 10
+b = 5
+print(f"Addition of a and b is: {a+b}")
+print("Addition of a and b is:",a+b)
+    
+a = int(input())
+b = int(input())
+print(f"Addition of a and b is: {a+b}")
+print("Addition of a and b is:",a+b)
+
+#area of circle
+
+r = float(input())
+area = 3.14*r*r
+print(f"Area of circle is:{area}")
+print("Area of circle is:",area,sep="")
